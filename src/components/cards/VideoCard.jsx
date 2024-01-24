fb73104253133b8ff61628e47e53c5b6720ac3ad
paulo.tavares <paulo.tavares@olxbr.com>
2024-01-24 09:05:57 -0300

    using jsx

--- src/components/cards/VideoCard.jsx
+++ src/components/cards/VideoCard.jsx
@@ -6,17 +6,13 @@ import { Play } from 'react-bootstrap-icons';
 export default function VideoCard(props){
     const [heartFilled, setHeartFilled] = useState(false)
 
-    const fillHeart = () => {
-        setHeartFilled(!heartFilled);
-    }
-
     return (
         <div className={styles.card}>
             <div className={styles.cardImage}>
                 <div className={styles.cardImageCover}>
                     <Play className={styles.playButton}></Play>
                 </div>
-                <img src={props.tumbnail} />
+                <img src={props.tumbnail} alt=''/>
             </div>
             <div className={styles.cardBody}>
                 <div className={styles.cardTitle}>
@@ -28,8 +24,8 @@ export default function VideoCard(props){
             </div>
             <div className={styles.classFooter}>
                 <div>{props.info}</div>
-                <div className={styles.cardFooterButton} onClick={fillHeart}>
-                    {{heartFilled} ? <Heart></Heart> : <HeartFill></HeartFill>}
+                <div className={styles.cardFooterButton} onClick={()=>setHeartFilled(!heartFilled)}>
+                    {!heartFilled ? <Heart></Heart> : <HeartFill></HeartFill>}
                 </div>
             </div>
         </div>
